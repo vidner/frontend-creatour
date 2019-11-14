ff40e4d5aef754056364352eda2e82419f69158c
vidner <valorousidner@gmail.com>
2019-11-14 09:24:20 +0700

    add everything

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
+
+import { IndexComponent } from "./pages/index/index.component";
+import { ProfilepageComponent } from "./pages/profilepage/profilepage.component";
+import { RegisterpageComponent } from "./pages/registerpage/registerpage.component";
+import { LandingpageComponent } from "./pages/landingpage/landingpage.component";
+import { LoginpageComponent } from "./pages/loginpage/loginpage.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "landing", component: IndexComponent },
+  { path: "profile", component: ProfilepageComponent },
+  { path: "register", component: RegisterpageComponent },
+  { path: "home", component: LandingpageComponent },
+  { path: "login", component: LoginpageComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
+  exports: []
+})
+export class AppRoutingModule {}
