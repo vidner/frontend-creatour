51d5edd1927f8be1131ec9c0365f274ae35c7d34
Vaddyl <14anonymous03@gmail.com>
2019-11-23 14:17:09 +0700

    project contributor total progress

--- src/app/pages/projectpage/projectpage.component.ts
+++ src/app/pages/projectpage/projectpage.component.ts
@@ -1,5 +1,6 @@
 import { Component ,OnInit, OnDestroy} from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
 import Chart from "chart.js";
 
 import { ProjectService } from "../../_services/project.service";
@@ -9,12 +10,21 @@ import { ProjectService } from "../../_services/project.service";
   templateUrl: "projectpage.component.html"
 })
 export class ProjectpageComponent    {
-  projects: any ;
+  projects: any;
   constructor(private http: HttpClient, private projectService: ProjectService) {
     this.projects = [];
   }
   ngOnInit(){
     this.projectService.getProjects()
+      .pipe(map(projects => projects.data.map(project => ({
+            id: project.id,
+            description: project.description,
+            name: project.name,
+            count: project.ProjectMembers.length,
+            ProjectMembers: project.ProjectMembers,
+            ProjectCategories: project.ProjectCategories,
+            filled: project.ProjectMembers.filter(obj=>!Object.values(obj).includes(null)).length
+          }))))
       .subscribe(projects => {
         this.projects = projects;
         console.log(this.projects);
